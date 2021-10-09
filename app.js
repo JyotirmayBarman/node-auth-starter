cf56ff68e4eadd12360d5391c4b3a79c3f56f4dc
Jyotirmay Barman <jyotirmaybarman.email@gmail.com>
2021-10-09 23:51:58 +0530

    add cors support

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const mainRouter = require('./routes/main.router');
 const cookie = require('cookie-parser');
-
+const cors = require('cors');
 const { errorHandler } = require('./middlewares/globalErrorHandler')
 
 const app = express();
+app.use(cors({
+    origin: 'http://localhost:8000',
+    credentials:true
+}));
 app.use(express.json());
 app.use(cookie());
 
