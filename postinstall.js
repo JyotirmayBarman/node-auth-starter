128034bb73be1148ae8c100df820999095519e82
Jyotirmay Barman <jyotirmaybarman.email@gmail.com>
2021-10-11 16:25:55 +0530

    fix post install script

---
+++ postinstall.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const {execSync} = require('child_process')
+if (!fs.existsSync('./.env')){
+    execSync("echo \"PORT=8000\n\nMONGO_URL=mongodb://localhost:27017/node_auth\n\nREDIS_PORT=6379\nREDIS_HOST='localhost'\n\nREFRESH_TOKEN_SECRET=M@iR3fr3sHt0KenS3cR3T\nVERIFY_EMAIL_SECRET=v3riFYem@@iLsssecret\n\nMAIL_ENV=smtp\nSMTP_HOST=your-smtp-relay\nSMTP_PORT=587\nSMTP_USERNAME=your@username\nSMTP_PASSWORD=K5cr2zqbBYA4fkZD\" > .env");
+}
+if (!fs.existsSync('./.gitignore')){
+    execSync("echo \"node_modules/\nuploads/\n.env\n.gitignore\" > .gitignore;")
+}
+if(!fs.existsSync('./uploads')){
+    execSync("mkdir uploads")
+}
