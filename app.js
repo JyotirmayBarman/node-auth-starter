946901e404c5413dab021f839575461a586e2c66
Jyotirmay Barman <jyotirmaybarman.email@gmail.com>
2021-10-18 13:43:22 +0530

    add support to change cors origins from .env

--- app.js
+++ app.js
@@ -6,11 +6,10 @@ const cookie = require('cookie-parser');
 const cors = require('cors');
 const { errorHandler } = require('./middlewares/globalErrorHandler')
 
-const PORT=process.env.PORT || 8000;
-
 const app = express();
+
 app.use(cors({
-    origin: `http://localhost:${PORT}`,
+    origin: process.env.CORS_URLS.split(','),
     credentials:true
 }));
 app.use(express.json());
