b8727ac657f80495c5d01150ff0aa0bbcf952eb3
Jyotirmay Barman <jyotirmaybarman.email@gmail.com>
2021-10-18 21:50:34 +0530

    fix mime validator

--- validators/mime.validator.js
+++ validators/mime.validator.js
@@ -1,23 +1,25 @@
-function validateImageInput(req,res,next){
-    const allowedMimeTypes = ['image/jpg','image/jpeg','image/png'];
-    
-    if(!req.file && req.method == 'PATCH'){
-        return next();
-    }
-    else if(!req.file){
-        return res.status(401).json({
-            error:"Image field should not be empty",
-            field:'image'
-        })
-    }
-    else if( req.file && allowedMimeTypes.includes(req.file.mimetype)){
-        next();
-    }
-    else{
-        return res.status(401).json({
-            error:"Only .png, .jpg, .jpeg files are supported",
-            field:'image'
-        })
+function validateImageInput(fieldName='image'){
+    return (req,res,next)=>{
+        const allowedMimeTypes = ['image/jpg','image/jpeg','image/png'];
+        
+        if(!req.file && req.method == 'PATCH'){
+            return next();
+        }
+        else if(!req.file){
+            return res.status(401).json({
+                error:`${fieldName[0].toUpperCase() + fieldName.substring(1)} field should not be empty`,
+                field:fieldName
+            })
+        }
+        else if( req.file && allowedMimeTypes.includes(req.file.mimetype)){
+            next();
+        }
+        else{
+            return res.status(401).json({
+                error:"Only .png, .jpg, .jpeg files are supported",
+                field:fieldName
+            })
+        }
     }
 }
 
